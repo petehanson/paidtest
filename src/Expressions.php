4a30456bda7c7a1d04539b52754b71a31d1bf797
Peter Hanson <peter@upandrunningsoftware.com>
2015-08-28 12:48:10 -0500

    added some addition and substraction support

--- src/Expressions.php
+++ src/Expressions.php
@@ -5,6 +5,67 @@ namespace Pete;
 class Expressions {
 
     public static function evaluate($expression) {
-        return 7;
+
+        $runningTotal = 0;
+
+        preg_match_all("/[\d\+\-]+?/",$expression,$matches);
+
+        $matches = $matches[0];
+        //print_r($matches);
+        //$matches = array_reverse($matches);
+        //print_r($matches);
+
+
+        $numbers = array();
+        $operators = array();
+
+
+        foreach ($matches as $index=>$element) {
+            if ($element == "+" || $element == '-') {
+                // is operator
+
+
+                array_push($operators, $element);
+            } else {
+                // is number
+
+                array_push($numbers,$element);
+
+
+            }
+        }
+
+        //print_r($numbers);
+        //print_r($operators);
+
+        while (count($operators) > 0) {
+            $op = array_pop($operators);
+
+            $num1 = array_pop($numbers);
+            $num2 = array_pop($numbers);
+
+            $result = self::doMath($num1,$num2,$op);
+            echo $result . "\n";
+            array_push($numbers,$result);
+        }
+
+
+
+        return array_pop($numbers);
+    }
+
+    protected function doMath($number1,$number2,$operator) {
+
+
+        switch ($operator) {
+            case "+":
+                return $number2 + $number1;
+                break;
+
+            case "-":
+                return $number2 - $number1;
+                break;
+        }
+
     }
 }
--- tests/testExpressions.php
+++ tests/testExpressions.php
@@ -7,7 +7,12 @@ use \Pete\Expressions as Expressions;
 class ExpressionsTest extends PHPUnit_Framework_TestCase
 {
     function testAdd() {
-        $this->assertEquals(7,Expressions::evaluate("2+7"));
+        $this->assertEquals(9,Expressions::evaluate("2+7"));
+        $this->assertEquals(10,Expressions::evaluate("2+7+1"));
+    }
+
+    function testSubtract() {
+        $this->assertEquals(3,Expressions::evaluate("7-4"));
     }
 
 }
